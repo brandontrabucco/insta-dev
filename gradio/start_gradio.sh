7d6f31aa15072761375ccea6c8e117f95931e66a
Brandon Trabucco <brandon@btrabucco.com>
2025-03-24 17:08:58 +0000

    update gradio launch script

--- gradio/start_gradio.sh
+++ gradio/start_gradio.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+export SERVER_DIR=${SERVER_DIR:-"javascript/server"}
+export SERVER_SCRIPT=${SERVER_SCRIPT:-"javascript/server/src/index.js"}
+export SERVER_LOG=${SERVER_LOG:-"playwright.log"}
+
+export SERVER_BASE_PORT=${SERVER_BASE_PORT:-3000}
+export SERVER_WORKERS=${SERVER_WORKERS:-8}
+export MAX_ERRORS=${MAX_ERRORS:-1000}
+
 source /miniconda3/bin/activate
 conda activate insta
 
-touch playwright.log && tail -f playwright.log &
+touch ${SERVER_LOG} && tail -f ${SERVER_LOG} &
 bash start_playwright_server.sh
 
 python gradio/app.py
