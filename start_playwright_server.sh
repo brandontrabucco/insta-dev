db40f81119fbdfa2332c4df9b854a221cb4d1f6e
Brandon Trabucco <brandon@btrabucco.com>
2025-03-24 16:51:01 +0000

    move server location to top level

--- start_playwright_server.sh
+++ start_playwright_server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-export SERVER_DIR=${SERVER_DIR:-"insta/javascript/server"}
-export SERVER_SCRIPT=${SERVER_SCRIPT:-"insta/javascript/server/src/index.js"}
+export SERVER_DIR=${SERVER_DIR:-"javascript/server"}
+export SERVER_SCRIPT=${SERVER_SCRIPT:-"javascript/server/src/index.js"}
+export SERVER_LOG=${SERVER_LOG:-"playwright.log"}
+
 export SERVER_BASE_PORT=${SERVER_BASE_PORT:-3000}
 export SERVER_WORKERS=${SERVER_WORKERS:-8}
 export MAX_ERRORS=${MAX_ERRORS:-1000}
@@ -11,7 +13,7 @@ read -r -d '' PLAYWRIGHT_COMMAND << END_OF_SCRIPT
 pushd ${SERVER_DIR} && npx tsc && popd
 
 seq 1 ${MAX_ERRORS} | xargs --process-slot-var WORKER_IDX -I {} -P ${SERVER_WORKERS} \
-    bash -c "node ${SERVER_SCRIPT} \\\$((WORKER_IDX + ${SERVER_BASE_PORT})) >> playwright.log 2>&1"
+    bash -c "node ${SERVER_SCRIPT} \\\$((WORKER_IDX + ${SERVER_BASE_PORT})) >> ${SERVER_LOG} 2>&1"
 
 END_OF_SCRIPT
 
